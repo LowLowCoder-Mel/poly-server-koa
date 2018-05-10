a08897fc891a9761512fe47fa8c72bafb2120e74
zhangxuesong <453025053@qq.com>
2018-05-10 16:06:16 +0800

    U update

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,14 @@ import {sendCaptcha, getUserList, register, removeUser} from '../app/controller/
 /**
  * 发送验证码
  */
-router.get('/send_captcha', async (ctx, next) => {
+router.get('send_captcha', async (ctx, next) => {
   ctx.body = await sendCaptcha(ctx, next);
 });
 
 /**
  * 注册用户
  */
-router.post('/register', async (ctx, next) => {
+router.post('register', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -22,7 +22,7 @@ router.post('/register', async (ctx, next) => {
 /**
  * 用户登陆
  */
-router.post('/login', async (ctx, next) => {
+router.post('login', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -31,7 +31,7 @@ router.post('/login', async (ctx, next) => {
 /**
  * 密码找回
  */
-router.post('/password/reset', async (ctx, next) => {
+router.post('password/reset', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -40,7 +40,7 @@ router.post('/password/reset', async (ctx, next) => {
 /**
  * 修改用户信息
  */
-router.post('/profile', async (ctx, next) => {
+router.post('profile', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -49,7 +49,7 @@ router.post('/profile', async (ctx, next) => {
 /**
  * 修改密码
  */
-router.post('/update_password', async (ctx, next) => {
+router.post('update_password', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -58,7 +58,7 @@ router.post('/update_password', async (ctx, next) => {
 /**
  * 创建家庭
  */
-router.post('/family', async (ctx, next) => {
+router.post('family', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -67,7 +67,7 @@ router.post('/family', async (ctx, next) => {
 /**
  * 修改家庭信息
  */
-router.post('/family_update', async (ctx, next) => {
+router.post('family_update', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -76,7 +76,7 @@ router.post('/family_update', async (ctx, next) => {
 /**
  * 删除家庭
  */
-router.post('/family_delete', async (ctx, next) => {
+router.post('family_delete', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -85,7 +85,7 @@ router.post('/family_delete', async (ctx, next) => {
 /**
  * 查询家庭信息
  */
-router.post('/family_query', async (ctx, next) => {
+router.post('family_query', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -94,7 +94,7 @@ router.post('/family_query', async (ctx, next) => {
 /**
  * 绑定家庭成员
  */
-router.post('/family_user_update', async (ctx, next) => {
+router.post('family_user_update', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -103,7 +103,7 @@ router.post('/family_user_update', async (ctx, next) => {
 /**
  * 删除家庭成员关系
  */
-router.post('/family_user_update', async (ctx, next) => {
+router.post('family_user_update', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -112,7 +112,7 @@ router.post('/family_user_update', async (ctx, next) => {
 /**
  * 删除家庭成员关系
  */
-router.post('/family_user_delete', async (ctx, next) => {
+router.post('family_user_delete', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -121,7 +121,7 @@ router.post('/family_user_delete', async (ctx, next) => {
 /**
  * 查询家庭所有成员关系
  */
-router.post('/family_user_queryusers', async (ctx, next) => {
+router.post('family_user_queryusers', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -130,7 +130,7 @@ router.post('/family_user_queryusers', async (ctx, next) => {
 /**
  * 查询该用户所在所有家庭
  */
-router.post('/family_user_queryusers', async (ctx, next) => {
+router.post('family_user_queryusers', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -139,7 +139,7 @@ router.post('/family_user_queryusers', async (ctx, next) => {
 /**
  * 主机绑定接口（本地）
  */
-router.post('/binding', async (ctx, next) => {
+router.post('binding', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -148,7 +148,7 @@ router.post('/binding', async (ctx, next) => {
 /**
  * 主机解绑接口（本地）
  */
-router.post('/unbinding', async (ctx, next) => {
+router.post('unbinding', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -157,7 +157,7 @@ router.post('/unbinding', async (ctx, next) => {
 /**
  * 图片上传Base64
  */
-router.post('/upload/image/base64', async (ctx, next) => {
+router.post('upload/image/base64', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -166,7 +166,7 @@ router.post('/upload/image/base64', async (ctx, next) => {
 /**
  * 修改用户头像
  */
-router.post('/update_cover', async (ctx, next) => {
+router.post('update_cover', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -175,7 +175,7 @@ router.post('/update_cover', async (ctx, next) => {
 /**
  * 修改用户头像
  */
-router.post('/discovery/list', async (ctx, next) => {
+router.post('discovery/list', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -184,7 +184,7 @@ router.post('/discovery/list', async (ctx, next) => {
 /**
  * 安防记录获取
  */
-router.post('/security_infos', async (ctx, next) => {
+router.post('security_infos', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await register(reqBody);
@@ -193,14 +193,14 @@ router.post('/security_infos', async (ctx, next) => {
 /**
  * 获取用户
  */
-router.get('/getUser', async (ctx, next) => {
+router.get('getUser', async (ctx, next) => {
   ctx.body = await getUserList(ctx, next);
 });
 
 /**
  * 删除用户
  */
-router.del('/removeUser', async (ctx, next) => {
+router.del('removeUser', async (ctx, next) => {
   console.log(ctx.request.body);
   let reqBody = ctx.request.body;
   ctx.body = await removeUser(reqBody);
